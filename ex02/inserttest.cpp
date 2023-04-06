fd6e7374488b3068b9111b4808b4e47d6266c50d
Namik Hwang <nhwang@c9r4s3.42seoul.kr>
2023-04-06 18:18:31 +0900

    merge MergeSort with InsertSort

--- ex02/inserttest.cpp
+++ ex02/inserttest.cpp
@@ -4,11 +4,20 @@
 int main() {
     std::vector<int> arr;
 
-    arr.push_back(1);
-    arr.push_back(1);
+//"3 2 1 5 89 9 67 890 67 45 89 56 32 54 46 7 78 87 897 789"
+// "3 2 1 5 89 9 67 890 67 45 89"
     arr.push_back(3);
-    arr.push_back(4);
-    arr.push_back(4);
+    arr.push_back(2);
+    arr.push_back(1);
+    arr.push_back(5);
+    arr.push_back(89);
+    arr.push_back(9);
+    arr.push_back(67);
+    arr.push_back(890);
+    arr.push_back(67);
+    arr.push_back(45);
+    arr.push_back(89);
+
     std::cout << "==========origin==========" << std::endl;
     for (int i = 0; i < arr.size(); i++)
         std::cout << arr[i] << " ";
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -29,11 +29,48 @@ void MakeSort(int st, int mid, int end, std::vector<int> &_origin) {
         st++;
     }
 }
+void InsertSort(std::vector<int> &arr, int st, int end)
+{
+    int stand;
+    int back_ind;
+
+    std::cout << "=====before InsertSort=====" << std::endl;
+    for (int i = st; i <= end; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+    ///??
+    for (int stand_ind = st + 1; stand_ind <= end; stand_ind++) {
+        back_ind = stand_ind;
+        stand = arr[stand_ind];
+        for (int curr = stand_ind - 1; curr >= st; curr--) //st!
+            if (stand < arr[curr]) {
+                arr[back_ind] = arr[curr];
+                back_ind--;
+            }
+        arr[back_ind] = stand;
+    }
+    ////check Insert////
+    std::cout << "=====after InsertSort=====" << std::endl;
+    std::cout << "st :" << st << std::endl;
+    std::cout << "end :" << end << std::endl;
+    for (int i = st; i <= end; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+    ////////////////////
+}
 // std::vector<int> MergeSort()
-void MergeSort(std::vector<int> &_origin, int st, int end) {
+void MergeSort(std::vector<int> &_origin, int st, int end) { //함수명 바꿔줄 것.
     int mid;
-    if (st == end) // deque로 처리할 것이라면 ex00서 빼기
+    if (end-st+1 <= 10)///길이가 10이하인 배열에 관하여... // deque로 처리할 것이라면 ex00서 빼기 //(st == end) k==10인 상황
+    {
+        InsertSort(_origin,st,end);
         return;
+    }
+
+    ////////////////MergeSort Only////////////////
+    // if (end==st)
+    //     return;
+    ////////////////MergeSort Only////////////////
 
     int _size = end - st + 1;
     mid = ((st + end) / 2);
@@ -69,3 +106,16 @@ int main(int argc, char *argv[]) {
         std::cout << origin[j] << " ";
     std::cout << std::endl;
 }
+
+//./a.out "3 2 1 5 89 9 67 890 67 45 89 56 32 54 46 7 78 87 897 789"
+//결과값 >>> 1 2 3 5 7 9 32 45 46 54 56 67 67 78 87 89 890 789 890 897
+//ㄴ>789이슈 있음
+
+//1 2 3 5 7 9 32 45 46 54 56 67 67 78 87 89 890 789 890 897
+//1 2 3 5 7 9 32 45 46 54 56 67 67 78 87 89 89 789 890 897 >>> 병합시에는 문제 없었음. 삽입정렬 구현부 확인 필요.
+//1 2 3 5 7 9 32 45 46 54 56 67 67 78 87 89 89 789 890 897
+
+
+
+/////./a.out "3 2 1 5 89 9 67 890 67 45 89"
+////ㄴ> 결과값 : 1 2 3 5 9 45 89 67 89 89 890
